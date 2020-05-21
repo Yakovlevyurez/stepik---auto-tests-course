9fd68a83be54230b8413ead9174f6dc581143df5
Yury Iakovlev <yakovlevyurez@gmail.com>
2020-05-21 11:57:46 +0300

    3.4_pytest_fixtures

---
+++ lesson_3_2_step13.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+import time
+import unittest
+
+
+class TestAbs(unittest.TestCase):
+    def test_reg1(self):
+
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element_by_css_selector("[placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector("[placeholder='Input your last name']")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector("[placeholder='Input your email']")
+        input3.send_keys("spliff@bk.ru")
+
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+        time.sleep(2)
+        browser.quit()
+
+    def test_reg2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element_by_css_selector("[placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector("[placeholder='Input your last name']")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector("[placeholder='Input your email']")
+        input3.send_keys("spliff@bk.ru")
+
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+        time.sleep(2)
+        browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
